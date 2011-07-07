9429a0450b77a743dc9d2701e082cfa06847f36b
Radamanthus Batnag <rad@batnag.org>
2011-07-07 08:10:15 +0800

    Added title to result of story web service

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -1,6 +1,6 @@
 class Api::StoriesController < ApplicationController
   def show
     story = Story.find_by_code_and_position(params[:code], params[:position])
-    render :text => story.body || '' 
+    render :text => {:body => story.body || '', :title => story.title}.to_json
   end
 end
